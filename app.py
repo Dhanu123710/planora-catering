ba351dcba7b9bb1d1370d6fe35d6128239187f5f
dhanush <dhanushsapaliga632@gmail.com>
2026-05-06 22:36:53 +0530

    Fix database initialization for Render

--- app.py
+++ app.py
@@ -16,6 +16,10 @@ login_manager = LoginManager()
 login_manager.login_view = 'login'
 login_manager.init_app(app)
 
+# Create tables if they don't exist
+with app.app_context():
+    db.create_all()
+
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
@@ -550,6 +554,4 @@ def complete_assignment(asgn_id):
     return jsonify({'success': True})
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
     app.run(debug=True)
